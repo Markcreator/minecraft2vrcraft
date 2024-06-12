b983cb09af6c6d7699a1f01c33fa4658a8feca6e
Mark <markmediahorn@hotmail.com>
2024-06-12 11:43:22 -0400

    default plugin setup

--- src/net/Markcreator/minecraft2vrcraft/Main.java
+++ src/net/Markcreator/minecraft2vrcraft/Main.java
@@ -1,5 +1,31 @@
 package net.Markcreator.minecraft2vrcraft;
 
-public class Main extends JavaPlugin {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin implements Listener, CommandExecutor {
+
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.getLogger().info(pdfFile.getName() + " has been enabled.");
+		
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(this, this);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (cmd.getName().equalsIgnoreCase("export")) {
+			sender.sendMessage("export");
+			return true;
+		}
+		
+		return false; 
+	}
 }
